4997001000d3c327688124aa64ba752fd4cd4cee
GEARLESS JOE <c1094282069@gmail.com>
2024-05-16 09:34:46 +0800

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </main>
+  );
+}
